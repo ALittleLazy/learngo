c40746c84f218a225275eb1331ab8b3e4de69146
ALittleLazy <980393804@qq.com>
2019-10-29 15:40:21 +0800

    test

--- MyTool/test.go
+++ MyTool/test.go
@@ -1,7 +1,24 @@
 package main
 
-func main() {
-	type fef map[string] string
+import "fmt"
 
+type ttype struct {
+	userid, password, remark string
+}
+
+func main() {
+	ip := make(map[string]map[string]ttype)
+	databasename := make(map[string]ttype)
+	databasename["test"] = ttype{"sa", "123", "shuoming"}
+	ip["127.0.0.1"] = databasename
+	fmt.Println(ip)
+
+	v, ok := ip["127.0.0.1"]["test"]
+
+	if ok == false {
+		fmt.Print("not find", ok)
+	} else {
+		fmt.Print("find:", v)
+	}
 
 }
